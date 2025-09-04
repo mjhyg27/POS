f0208b1f1212cd2720dc2684421f30be14fd4e23
Miguel <miguel_romo02@hotmail.com>
2025-09-03 18:09:07 -0700

    se trabajo en los modals

---
+++ posScript.js
@@ -0,0 +1,12 @@
+let closeSaleButton = document.getElementById('close-sale-button');
+
+closeSaleButton.addEventListener('click', closeSaleModal);
+
+function closeSaleModal() {
+    let modal = document.getElementById('close-sale');
+    modal.showModal();
+}
+
+function openModal() {
+    let modal = document.getElementById
+}
